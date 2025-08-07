9e985ea191182b52ff4f354b4fab7812f5dc3941
Abad Ali <abad02597@gmail.com>
2025-08-07 20:26:39 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-gray-950 text-gray-500 p-8'>
-      <div className='flex justify-center text-xs md:text-base'>Thanks for visiting! | Customer Support: [<a className="hover:text-blue-700" href="https://github.com/Abad-Ali">GitHub</a>]] </div>
+      <div className='flex justify-center text-xs md:text-base'>Thanks for visiting! | Customer Support: [<a className="hover:text-blue-700" href="https://github.com/Abad-Ali" target="_blank">GitHub</a>] </div>
       <div className='flex justify-center'>
           <div >© 2023
                 <span className='text-blue-700'> &lt;</span>
